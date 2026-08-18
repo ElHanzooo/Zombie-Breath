af8548886fd9d0beaf3959a5a6ab2a959bc9e85f
hanzo <20jndrsn08@gmail.com>
2026-08-18 14:29:41 -0300

    criei os métodos de entrada, saída e atualização do estado

--- Components/Finite State Machine/State.cs
+++ Components/Finite State Machine/State.cs
@@ -5,4 +5,12 @@ using System;
 public partial class State : Node
 {
     [Signal] public delegate void SwitchStateEventHandler(State state);
+
+    public void Enter() { }
+
+    public void Exit() { }
+
+    public void Update(double delta) { }
+
+    public void PhysicsUpdate(double delta) { }
 }
