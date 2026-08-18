002130253bdb33792f3681373c0e861f08947a31
hanzo <20jndrsn08@gmail.com>
2026-08-18 18:48:59 -0300

    criei o método PlaySoundEffect. Ele é usado para tocar um efeito sonoro (ou qualquer tipo de som desejado)

--- Global/Global.cs
+++ Global/Global.cs
@@ -6,4 +6,18 @@ public partial class Global : Node
     public static Global Instance { get; private set; }
 
     public override void _Ready() => Instance = this;
+
+    public void PlaySoundEffect(AudioStream sfx)
+    {
+        AudioStreamPlayer audioStreamPlayer = new()
+        {
+            Stream = sfx
+        };
+
+        audioStreamPlayer.Finished += () => QueueFree();
+        
+        AddChild(audioStreamPlayer);
+
+        audioStreamPlayer.Play();
+    }
 }
