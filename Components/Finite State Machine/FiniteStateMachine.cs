7590f2d07ddd81d05136c8c4b2517b5e3cc3545f
hanzo <20jndrsn08@gmail.com>
2026-08-18 14:34:57 -0300

    criei a variável Initial State e Active State

--- Components/Finite State Machine/FiniteStateMachine.cs
+++ Components/Finite State Machine/FiniteStateMachine.cs
@@ -1,6 +1,10 @@
 using Godot;
 using System;
 
+[GlobalClass]
 public partial class FiniteStateMachine : Node
 {
+    [Export] public State InitialState { get; set; }
+
+    public State ActiveState { get; set; }
 }
