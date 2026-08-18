85eeb1e0667903b068fc85a9aa775eea4b2eac02
hanzo <20jndrsn08@gmail.com>
2026-08-18 14:50:32 -0300

    ao entrar em cena, itera por todos os estados-filhos e conecta o sinal Switch State ao método Change State

--- Components/Finite State Machine/FiniteStateMachine.cs
+++ Components/Finite State Machine/FiniteStateMachine.cs
@@ -7,4 +7,14 @@ public partial class FiniteStateMachine : Node
     [Export] public State InitialState { get; set; }
 
     public State ActiveState { get; set; }
+
+    public override void _Ready()
+    {
+        foreach (State state in GetChildren())
+        {
+            state.Connect(State.SignalName.SwitchState, Callable.From<State>(ChangeState));
+        }
+    }
+
+    public void ChangeState(State newState) { }
 }
