68488005e9816a3fe7064c8395c589af3871abf0
hanzo <20jndrsn08@gmail.com>
2026-08-18 16:01:59 -0300

    durante a atualização do estado Idle, ele checa se um certo evento foi acionado. Caso este seja, troca para o estado Shoot

--- Characters/Mike/States/Idle.cs
+++ Characters/Mike/States/Idle.cs
@@ -3,6 +3,8 @@ using System;
 
 public partial class Idle : State
 {
+    [Export] private State ShootState { get; set; }
+
     private AnimatedSprite2D animations;
 
     public override void Enter()
@@ -10,5 +12,13 @@ public partial class Idle : State
         animations = GetNode<AnimatedSprite2D>("../../Animations");
 
         animations.Play("Idle");
+    }
+
+    public override void Update(double delta)
+    {
+        if (Input.IsActionJustPressed("Shoot"))
+        {
+            EmitSignal(SignalName.SwitchState, ShootState);
+        }
     }
 }
