ab977e5a882eaff90ceb24becf1ef13f1b4d5a6d
hanzo <20jndrsn08@gmail.com>
2026-08-21 16:52:05 -0300

    implementei os efeitos sonoros, inicializei a lista e completei o handle de animação

--- Components/Animation Manager/AnimationManager.cs
+++ Components/Animation Manager/AnimationManager.cs
@@ -7,16 +7,34 @@ public partial class AnimationManager : Node
     [Export] public string StandardAnimation { get; set; }
     [Export] private AnimatedSprite2D AnimatedSprite2DNode { get; set; }
 
-    private readonly Stack<(string animation, AudioStream audio)> animations;
+    private readonly Stack<(string animation, AudioStream? audio)> animations = new();
 
-    public void AddAnimation(string animation, AudioStream audio) => animations.Push((animation, audio));
+    public override void _Ready()
+    {
+        AnimatedSprite2DNode.AnimationFinished += PlayAnimation;
+
+        PlayAnimation();
+    }
+
+    public void AddAnimation(string animation, AudioStream? audio)
+    {
+        animations.Push((animation, audio));
+
+        if (AnimatedSprite2DNode.Animation == StandardAnimation)
+            PlayAnimation();
+    }
 
     private void PlayAnimation()
     {
-        var animation = animations.Pop();
-
         if (animations.Count > 0)
+        {
+            var animation = animations.Pop();
+
             AnimatedSprite2DNode.Play(animation.animation);
+
+            if (animation.audio != null)
+                Global.Instance.PlaySoundEffect(animation.audio);
+        }
         else
             AnimatedSprite2DNode.Play(StandardAnimation);
     }
