5e4484abcd09e32d25f8990c2ce8c21bfc1f86e2
hanzo <20jndrsn08@gmail.com>
2026-08-21 16:37:58 -0300

    criei dois métodos: AddAnimation, para adicionar as animações na lista; e PlayAnimation, para tocar estas animações (ou a animação padrão, se não tiver nenhuma)

--- Components/Animation Manager/AnimationManager.cs
+++ Components/Animation Manager/AnimationManager.cs
@@ -5,6 +5,19 @@ using System.Collections.Generic;
 public partial class AnimationManager : Node
 {
     [Export] public string StandardAnimation { get; set; }
+    [Export] private AnimatedSprite2D AnimatedSprite2DNode { get; set; }
 
     private readonly Stack<(string animation, AudioStream audio)> animations;
+
+    public void AddAnimation(string animation, AudioStream audio) => animations.Push((animation, audio));
+
+    private void PlayAnimation()
+    {
+        var animation = animations.Pop();
+
+        if (animations.Count > 0)
+            AnimatedSprite2DNode.Play(animation.animation);
+        else
+            AnimatedSprite2DNode.Play(StandardAnimation);
+    }
 }
