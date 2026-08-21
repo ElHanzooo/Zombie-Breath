d6f6f314b76c9861d34263403cb62f424c0d6d94
hanzo <20jndrsn08@gmail.com>
2026-08-21 16:55:19 -0300

    implementei o atributo GlobalClass

--- Components/Animation Manager/AnimationManager.cs
+++ Components/Animation Manager/AnimationManager.cs
@@ -2,6 +2,7 @@ using Godot;
 using System;
 using System.Collections.Generic;
 
+[GlobalClass]
 public partial class AnimationManager : Node
 {
     [Export] public string StandardAnimation { get; set; }
