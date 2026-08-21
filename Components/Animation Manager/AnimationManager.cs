66cb9fd924ee975a2657140a65e49e1ae28a2b43
hanzo <20jndrsn08@gmail.com>
2026-08-21 16:30:37 -0300

    criei a propriedade StandardAnimation. Ele é usado para definir a animação padrão que vai tocar quando não há nenhuma pendente. Também criei a lista que armazena estas animações pendentes

--- Components/Animation Manager/AnimationManager.cs
+++ Components/Animation Manager/AnimationManager.cs
@@ -1,6 +1,10 @@
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class AnimationManager : Node
 {
+    [Export] public string StandardAnimation { get; set; }
+
+    private readonly Stack<(string animation, AudioStream audio)> animations;
 }
